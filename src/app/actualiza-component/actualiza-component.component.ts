1b8bc600e36a599831edf9fd6891ba39dc1dd9d0
Jaime Díez <jaimecuentacod@gmail.com>
2023-05-12 09:25:46 +0200

    28. Routing V

--- src/app/actualiza-component/actualiza-component.component.ts
+++ src/app/actualiza-component/actualiza-component.component.ts
@@ -19,7 +19,15 @@ export class ActualizaComponentComponent {
     this.indice = this.route.snapshot.params['id'];
 
     let empleado: Empleado = this.empleadosService.encontrarEmpleado(this.indice);
-    
+
+    this.cuadroNombre = empleado.nombre;
+
+    this.cuadroApellido = empleado.apellido;
+
+    this.cuadroCargo = empleado.cargo;
+
+    this.cuadroSalario = empleado.salario;
+
   }
 
   empleados: Empleado[] = [];
@@ -30,9 +38,10 @@ export class ActualizaComponentComponent {
   }
 
 
-  actualizarEmpleado(){
+  actualizaEmpleado(){
     let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
-    this.empleadosService.actualizarEmpleadoServicio(miEmpleado);
+    this.empleadosService.actualizarEmpleado(this.indice, miEmpleado);
+    this.router.navigate(['']);
   }
 
   cuadroNombre: string = "";
@@ -40,6 +49,6 @@ export class ActualizaComponentComponent {
   cuadroCargo: string = "";
   cuadroSalario: number = 0;
 
-  indice: number | undefined;
+  indice!: number;
 
 }
