06ca3173977350ef2bcefcc8eaf9a5b4825f68a2
Jaime Díez <jaimecuentacod@gmail.com>
2023-05-12 08:42:23 +0200

    26. Routing III

--- src/app/proyectos-component/proyectos-component.component.ts
+++ src/app/proyectos-component/proyectos-component.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
+import { EmpleadosService } from '../empleados.service';
+import { Empleado } from '../empleado.model';
 
 @Component({
   selector: 'app-proyectos-component',
   templateUrl: './proyectos-component.component.html',
   styleUrls: ['./proyectos-component.component.css']
 })
-export class ProyectosComponentComponent {
+export class ProyectosComponentComponent implements OnInit{
+
+  constructor(private router: Router, private miServicio: ServicioEmpleadosService, private empleadosService: EmpleadosService){  }
+
+  ngOnInit(): void {
+    this.empleados = this.empleadosService.empleados;
+  }
+
+  empleados: Empleado[] = [];
+
+
+  volverHome(){
+    this.router.navigate([""])
+  }
+
+  agregarEmpleado(){
+    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    // this.miServicio.muestraMensaje("Nombre del empleado: " + miEmpleado.nombre);  
+    this.empleadosService.agregarEmpleadoServicio(miEmpleado);
+    this.router.navigate([""])
+  }
+
+  cuadroNombre: string = "";
+  cuadroApellido: string = "";
+  cuadroCargo: string = "";
+  cuadroSalario: number = 0;
 
 }
